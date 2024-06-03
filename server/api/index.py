b36e540c4002980c5cafe81c40bf7c8e965c1fb9
qwe515678 <mit.levkin@vk.com>
2024-06-03 22:07:03 +0300

    att 3

--- server/api/index.py
+++ server/api/index.py
@@ -1,40 +1,32 @@
-# from flask import Flask, request, jsonify
-# import json
-# from flask_cors import CORS
-# import threading
-# app = Flask(__name__)
-# CORS(app)
+from flask import Flask, request, jsonify
+import json
+from flask_cors import CORS
+import threading
+
+app = Flask(__name__)
+CORS(app)
 
 
-# @app.route("/api/health", methods=["GET"])
-# def healthchecker():
-#     return {"status": "success", "message": "Integrate Flask Framework with Next.js"}
+@app.route("/api/health", methods=["GET"])
+def healthchecker():
+    return {"status": "success", "message": "Integrate Flask Framework with Next.js"}
 
 
-# @app.route("/api/data", methods=["GET"])
-# def send_data():
-#     try:
-#         with open("api/data.json", "r", encoding="utf-8") as file:
-#             data = json.load(file)
-#         return jsonify(data), 200
-#     except FileNotFoundError:
-#         return jsonify({"error": "File not found"}), 404
+@app.route("/api/data", methods=["GET"])
+def send_data():
+    try:
+        with open("api/data.json", "r", encoding="utf-8") as file:
+            data = json.load(file)
+        return jsonify(data), 200
+    except FileNotFoundError:
+        return jsonify({"error": "File not found"}), 404
 
 
-# def run_app():
-#     app.run()
-# if __name__ == "__main__":
-#     server_thread = threading.Thread(target=run_app)
-#     server_thread.start()
+def run_app():
+    app.run(
+        debug=True,
+    )
 
 
-from http.server import BaseHTTPRequestHandler
-
-
-class handler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'text/plain')
-        self.end_headers()
-        self.wfile.write(str('Hello World!!').encode())
-        return
+if __name__ == "__main__":
+    run_app()
