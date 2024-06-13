49c8fbbd4b0c4f3851c652c5dd999802bb63e5fa
Дмитрий Левкин <mit.levkin@vk.com>
2024-06-13 16:39:57 +0300

    connected frontend and backend 🎇

--- client/src/components/Еxpand.tsx
+++ client/src/components/Еxpand.tsx
@@ -11,15 +11,14 @@ interface IExpand {
 export default function Expand({ children, maxHeight = 200 }: IExpand) {
   const [expanded, setExpanded] = useState(false);
   const containerRef = useRef<null | HTMLDivElement>(null);
-  useEffect(() => {
-    const id = setInterval(() => {
-      if (expanded){
-
-        setExpanded((containerRef.current?.offsetHeight ?? 0) < 2 * maxHeight);
-      }
-    }, 500);
-    return () => clearInterval(id);
-  }, []);
+  // useEffect(() => {
+  //   const id = setInterval(() => {
+  //     if (expanded){
+  //       setExpanded((containerRef.current?.offsetHeight ?? 0) < 2 * maxHeight);
+  //     }
+  //   }, 500);
+  //   return () => clearInterval(id);
+  // }, []);
   return (
     <div className="relative w-full">
       <motion.div
@@ -42,17 +41,21 @@ export default function Expand({ children, maxHeight = 200 }: IExpand) {
           {children}
         </div>
       </motion.div>
-
+        {(containerRef.current?.offsetHeight??0) >  2 * maxHeight && (
+          <>
+          
       <div
         className={`pointer-events-none absolute bottom-0 left-0 right-0 h-10 bg-gradient-to-t from-background to-background/5 ${expanded ? "opacity-0" : "opacity-100"} transition`}
       ></div>
 
       <div
-        className={`absolute -bottom-9 left-1/2 -translate-x-1/2 cursor-pointer rounded-xl border border-border/75 bg-background px-2 py-1 text-sm `}
+        className={`absolute -bottom-9 left-1/2 -translate-x-1/2 cursor-pointer rounded-xl border border-border/75 bg-background px-2 py-1 text-sm`}
         onClick={() => setExpanded(!expanded)}
       >
         {expanded ? "свернуть" : "еще"}
       </div>
+          </>
+        )}
     </div>
   );
 }
